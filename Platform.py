4a9edb06fcb3ffa8c319c19d096e97617fcf97dc
Nightsunnv <zhouych@shanghaitech.edu.cn>
2022-05-17 12:05:50 +0800

    tinyChange

--- Platform.py
+++ Platform.py
@@ -5,6 +5,7 @@ class Platform():
 
     def __init__ (self):
         self.kind = 0
+        self.vel = 0
         self.green = pygame.transform.scale(pygame.image.load("assets/green.png"), (80,25)).convert_alpha()             # Green Platform
         self.blue = pygame.transform.scale(pygame.image.load("assets/blue.png"), (80,25)).convert_alpha()                # Blue Moving Platform
         self.red = pygame.transform.scale(pygame.image.load("assets/red.png"), (80,25)).convert_alpha()                 # Red Fragile Platform
@@ -28,21 +29,24 @@ class Platform():
             if (chance < 85):                 # 80% chance to get green platform
                 self.kind = 0            
             elif (chance < 95):               # 10% chance to get red or blue 
-                self.kind = 1                   
+                self.kind = 1  
+                self.vel = 5                 
             else:
                 self.kind = 2  
         elif (score < 2500):
             if (chance < 75):                 # 70% chance to get green platform
                 self.kind = 0            
             elif (chance < 95):               # 20% chance to get blue 
-                self.kind = 1                   
+                self.kind = 1   
+                self.vel = 5                
             else:
                 self.kind = 2 
         else:
             if (chance < 55):                 # 50% chance to get green platform
                 self.kind = 0            
             elif (chance < 95):               # 40% chance to get blue 
-                self.kind = 1                   
+                self.kind = 1  
+                self.vel = 8                 
             else:
                 self.kind = 2                   # 10% chance to get red
 
@@ -54,18 +58,14 @@ class Platform():
 
     def blueMovement(self, score):
         x = 0
-        if (score < 2500):
-            vel = 5
-        else:
-            vel = 8
                 
         if (self.blueDirection > 0):
-            x += vel
+            x += self.vel
             self.x += x
             if (self.x >= 600 - self.blue.get_width()):
                 self.blueDirection = 0
         else:      
-            self.x -= vel
+            self.x -= self.vel
             
             if (self.x <= 0):
                 self.blueDirection = 1
